4a04ab22befae63c0005bcaeba40775a273ccdf1
tomshelly-ship-it <73125371+tomshelly-ship-it@users.noreply.github.com>
2020-12-11 20:50:24 +0200

    Added switch to Fahr and icon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ function showTemp(response) {
   let temperature = `${Math.round(response.data.main.temp)}`;
   let tempNow = document.querySelector("#cel");
   tempNow.innerHTML = ` its ${temperature}℃`;
+  let changeCity = document.querySelector(".display-2");
+  changeCity.innerHTML = `It's going to be ${response.data.weather[0].description} in ${response.data.name}`;
+  let wind = document.querySelector("#speed");
+  wind.innerHTML = `Wind speed: ${Math.round(response.data.wind.speed)}km/h`;
+  let humidity = document.querySelector("#prec");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
+  let changeIcon = document.querySelector(`#icon`);
+  changeIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  changeIcon.setAttribute("alt", `${response.data.weather[0].description}`);
 }
 
 function formatDate(date) {
@@ -18,13 +31,19 @@ live.innerHTML = formatDate(new Date());
 function city(event) {
   event.preventDefault();
   let fillCity = document.querySelector("#inlineFormInput");
-  let changeCity = document.querySelector(".display-2");
-  let description = document.querySelector(".display-2");
-  changeCity.innerHTML = `It's going to be ${description.value} in ${fillCity.value}`;
-  description.innerHTML = response.data.weather[0].description;
+
   let apiKey = "a4a59d6f5e1d126fd66ccd575a9311a3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${fillCity.value}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
 let tell = document.querySelector("#city-form");
 tell.addEventListener("submit", city);
+
+function fahrTemp(event) {
+  event.preventDefault();
+  let fahrenheit = Math.round(temperature * (9 / 5) + 32);
+  document.querySelector("#cel").innerHTML = `${fahrenheit}°F`;
+}
+
+let fahrenheitButton = document.querySelector(`.fahrenheit`);
+fahrenheitButton.addEventListener("click", fahrTemp);
